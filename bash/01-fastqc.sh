89ad30f8e809ef802ae737ba28fd2624422f486a
Adil Hannaoui Anaaoui <154902011+AdilHannaoui@users.noreply.github.com>
2026-01-25 20:37:36 +0100

    Update 01-fastqc.sh

--- bash/01-fastqc.sh
+++ bash/01-fastqc.sh
@@ -6,11 +6,6 @@ set -euo pipefail
 # Author: Adil Hannaoui Anaaoui
 # ==========================
 
-WORKDIR="/mnt/c/Users/rna-seq/"
-FASTQ_DIR="$WORKDIR/data/"
-OUTPUT_DIR="$WORKDIR/output"
-THREADS=8
-
 mkdir -p "$OUTPUT_DIR/fastqc_results"
 mkdir -p "$OUTPUT_DIR/logs"
 
